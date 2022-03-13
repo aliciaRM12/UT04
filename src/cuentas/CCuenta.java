a8141f08b86e7c2d60d6eafdd2bfa1cb3cd63bf8
aliciaRM12 <alicia9112@hotmail.com>
2022-03-13 11:43:49 +0100

    añadimos comentarios

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,6 +1,17 @@
 package cuentas;
 
+
+/**
+ * 
+ * @author Alicia
+ *@since 13/03/2022
+ *
+ */
 public class CCuenta {
+	
+	/**
+	 *  Esta clase establece los parametros ce creacion de CCuenta
+	 */
 	
 	private String nombre;
     private String cuenta;
@@ -17,11 +28,26 @@ public class CCuenta {
         setCuenta(cue);
         setSaldo(sal);
     }
+    
+    /**
+     * 
+     * @param estado
+     * @return devuelve el valor alctual del saldo
+     * 
+     */
 
     public double estado()
     {
         return getSaldo();
     }
+    
+    /**
+     * 
+     * Realiza el ingreso de dienro en la cuenta
+     * @param cantidad
+     * @throws Exception, tiene que cumplir las condiciones de ser mayor a 0.
+     * 
+     */
 
     public void ingresar(double cantidad) throws Exception
     {
@@ -29,6 +55,14 @@ public class CCuenta {
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    
+    /**
+     * 
+     * Realiza la retirada de dinero de la cuenta
+     * @param cantidad
+     * @throws Exception, tiene que cumplir la condicion de ser mayor a 0.
+     * 
+     */
 
     public void retirar(double cantidad) throws Exception
     {
@@ -39,6 +73,13 @@ public class CCuenta {
         setSaldo(getSaldo() - cantidad);
     }
 
+    /**
+     * 
+     * Medotos getter y setter de nombre
+     * @param nombre 
+     * @return establece y muestra el nombre
+     * 
+     */
 	private String getNombre() {
 		return nombre;
 	}
@@ -46,6 +87,14 @@ public class CCuenta {
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * 
+	 * Metodos getter y setter de cuenta
+	 * @param cuenta
+	 * @return establece y devuelve el numero de cuenta
+	 * 
+	 */
 
 	private String getCuenta() {
 		return cuenta;
@@ -54,6 +103,14 @@ public class CCuenta {
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
+	
+	/**
+	 * 
+	 * Metodos getter y setter de saldo
+	 * @param saldo
+	 * @return devuelve el valor del saldo de la cuenta
+	 * 
+	 */
 
 	private double getSaldo() {
 		return saldo;
@@ -62,6 +119,14 @@ public class CCuenta {
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
+	
+	/**
+	 * 
+	 * Metodos getter y setter del tipo de ineteres
+	 * @param tipoInteres
+	 * @return devuelve el valor que tiene el tipo de interes
+	 * 
+	 */
 
 	private double getTipoInterés() {
 		return tipoInterés;
